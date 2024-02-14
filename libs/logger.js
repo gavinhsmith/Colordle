d3190586e15b1e7bd8e34a616d10fcff086636ea
Gavin Smith <39228744+gavinhsmith@users.noreply.github.com>
2024-02-14 10:19:30 -0600

    Added dates to logfile names.

--- libs/logger.js
+++ libs/logger.js
@@ -3,14 +3,18 @@ const winston = require("winston");
 const config = require("../config");
 const path = require("path");
 
+// Date
+const date = new Date();
+const date_str = `${date.getMonth()}-${date.getDate()}-${date.getFullYear()}_${date.getHours()}-${date.getMinutes()}-${date.getSeconds()}`;
+
 // Default/Main Logger
 const server_logger = winston.createLogger({
     level: (config.mode === "production") ? "info" : "debug",
     format: winston.format.json(),
     defaultMeta: {},
     transports: [
-        new winston.transports.File({ filename: path.join(__dirname, `../logs/error.log`), level: 'error' }),
-        new winston.transports.File({ filename: path.join(__dirname, `../logs/all.log`) }),
+        new winston.transports.File({ filename: path.join(__dirname, `../logs/${date_str}_error.log`), level: 'error' }),
+        new winston.transports.File({ filename: path.join(__dirname, `../logs/${date_str}_all.log`) }),
     ],
 });
 if (config.mode !== "production") {
